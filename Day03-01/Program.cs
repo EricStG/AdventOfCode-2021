704621cadd0420e48129487cafd4dae184cb5099
Eric St-Georges <eric.st-georges@hiloenergie.com>
2021-12-03 17:28:52 -0500

    Day 3 (ish)

---
+++ Day03-01/Program.cs
@@ -0,0 +1,44 @@
+﻿var content = File.ReadAllLines("input.txt");
+
+var cols = content[0].Length;
+
+var counts = new (int Ones, int Zeros)[cols];
+
+foreach (var row in content)
+{
+    for (var col = 0; col < cols; col++)
+    {
+        if (row[col] == '1')
+        {
+            counts[col].Ones++;
+        }
+        else
+        {
+            counts[col].Zeros++;
+        }
+    }
+}
+
+var gamma = new char[cols];
+var epsilon = new char[cols];
+
+for (var col = 0; col < cols; col++)
+{
+    var (ones, zeroes) = counts[col];
+
+    if (ones > zeroes)
+    {
+        gamma[col] = '1';
+        epsilon[col] = '0';
+    }
+    else
+    {
+        gamma[col] = '0';
+        epsilon[col] = '1';
+    }
+}
+
+var intGamma = Convert.ToInt64(new string(gamma), 2);
+var intEpsilon = Convert.ToInt64(new string(epsilon), 2);
+
+Console.WriteLine(intGamma * intEpsilon);
