c27c11689cc8b58303b4f922ff17990d072e6b68
Eric St-Georges <gh@thevedge.net>
2021-12-03 20:23:37 -0500

    Finished day 3

--- Day03-02/Program.cs
+++ Day03-02/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+
+        var content = File.ReadAllLines("input.txt");
+        
+        var o2gen = FindTarget(content, false);
+        var co2scrub = FindTarget(content, true);
+
+        Console.WriteLine(Convert.ToInt64(o2gen, 2) * Convert.ToInt64(co2scrub, 2));
+    }
+
+    private static string FindTarget(string[] content, bool reverseTarget)
+    {
+        var col = 0;
+
+        while (content.Length > 1)
+        {
+            var oneCount = 0;
+            var zeroCount = 0;
+
+            foreach (var line in content)
+            {
+                if (line[col] == '0')
+                {
+                    zeroCount++;
+                }
+                else
+                {
+                    oneCount++;
+                }
+            }
+
+            var target = oneCount >= zeroCount ^ reverseTarget? '0' : '1';
+            content = content.Where(c => c[col] == target).ToArray();
+            col++;
+        }
+
+        return content[0];
+    }
+}
