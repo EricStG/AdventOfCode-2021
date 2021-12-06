498309137bdcddd001ca4b48eda4c120b9785896
Eric St-Georges <gh@thevedge.net>
2021-12-06 18:08:16 -0500

    Day 6

---
+++ Day06-01/Program.cs
@@ -0,0 +1,21 @@
+﻿var content = File.ReadAllText("input.txt");
+
+var state = content.Split(',').Select(x => int.Parse(x)).ToList();
+
+const int days = 80;
+
+for (var day = 0; day < days; day++)
+{
+    var count = state.Count;
+    for (var i = 0; i < count; i++)
+    {
+        var current = --state[i];
+        if (current < 0)
+        {
+            state[i] = 6;
+            state.Add(8);
+        }
+    }
+}
+
+Console.WriteLine(state.Count);
---
+++ Day06-02/Program.cs
@@ -0,0 +1,27 @@
+﻿var content = File.ReadAllText("input.txt");
+var originalState = content.Split(',').Select(x => byte.Parse(x)).ToList();
+
+const int days = 256;
+
+var state = new long[9];
+
+foreach (var item in originalState)
+{
+    state[item]++;
+}
+
+for (var day = 0; day < days; day++)
+{
+    var reset = state[0];
+    for (var i = 1; i <= 8; i++)
+    {
+        state[i - 1] = state[i];
+    }
+
+    state[6] += reset;
+    state[8] = reset;
+}
+
+
+var count = state.Sum(x => x);
+Console.WriteLine(count);
