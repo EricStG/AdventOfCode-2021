75961b7f3b3b77b075366468f6863fb4c116ec99
Eric St-Georges <gh@thevedge.net>
2021-12-07 17:47:59 -0500

    Day 7

---
+++ Day07-01/Program.cs
@@ -0,0 +1,25 @@
+﻿var content = File.ReadAllText("input.txt");
+var originalState = content.Split(',').Select(x => int.Parse(x)).ToList();
+
+var max = originalState.Max();
+
+var minFuel = int.MaxValue;
+
+for (var i = 0; i <= max; i++)
+{
+    var fuel = 0;
+
+    foreach (var item in originalState)
+    {
+        if (i == item) continue;
+
+        fuel += Math.Abs(i - item);
+    }
+
+    if (fuel < minFuel)
+    {
+        minFuel = fuel;
+    }
+}
+
+Console.WriteLine(minFuel);
---
+++ Day07-02/Program.cs
@@ -0,0 +1,32 @@
+﻿var content = File.ReadAllText("input.txt");
+var originalState = content.Split(',').Select(x => int.Parse(x)).ToList();
+
+var max = originalState.Max();
+
+var minFuel = int.MaxValue;
+
+
+int GetSum(int value)
+{
+    return Enumerable.Range(1, value).Sum();
+}
+
+
+for (var i = 0; i <= max; i++)
+{ 
+    var fuel = 0;
+
+    foreach (var item in originalState)
+    {
+        if (i == item) continue;
+
+        fuel += GetSum(Math.Abs(i - item));
+    }
+
+    if (fuel < minFuel)
+    {
+        minFuel = fuel;
+    }
+}
+
+Console.WriteLine(minFuel);
