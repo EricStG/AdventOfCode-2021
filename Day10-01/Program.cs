3e9a05cffbf903433a25f97f1076ba4118bbac69
Eric St-Georges <gh@thevedge.net>
2021-12-11 01:44:09 -0500

    Day 10

---
+++ Day10-01/Program.cs
@@ -0,0 +1,50 @@
+﻿var content = File.ReadAllLines("input.txt");
+
+var pointMap = new Dictionary<char, int>
+{
+    { ')', 3 },
+    { ']', 57 },
+    { '}', 1197 },
+    { '>', 25137 },
+};
+
+var startMap = new Dictionary<char, char>
+{
+    { '(', ')' },
+    { '[', ']' },
+    { '{', '}' },
+    { '<', '>' },
+};
+
+var endMap = new Dictionary<char, char>
+{
+    { ')', '(' },
+    { ']', '[' },
+    { '}', '{' },
+    { '>', '<' },
+};
+
+var points = 0;
+
+foreach (var line in content)
+{
+    var chunks = new Stack<char>();
+
+    foreach(var x in line)
+    {
+        if (startMap.ContainsKey(x))
+        {
+            chunks.Push(x);
+            continue;
+        }
+         
+        var startChar = chunks.Pop();
+        if (startChar != endMap[x])
+        {
+            points += pointMap[x];
+            break;
+        }
+    }
+}
+
+Console.WriteLine(points);
---
+++ Day10-02/Program.cs
@@ -0,0 +1,64 @@
+﻿var content = File.ReadAllLines("input.txt");
+
+var pointMap = new Dictionary<char, int>
+{
+    { ')', 1 },
+    { ']', 2 },
+    { '}', 3 },
+    { '>', 4 },
+};
+
+var startMap = new Dictionary<char, char>
+{
+    { '(', ')' },
+    { '[', ']' },
+    { '{', '}' },
+    { '<', '>' },
+};
+
+var endMap = new Dictionary<char, char>
+{
+    { ')', '(' },
+    { ']', '[' },
+    { '}', '{' },
+    { '>', '<' },
+};
+
+var scores = new List<long>();
+
+foreach (var line in content)
+{
+    var chunks = new Stack<char>();
+    bool invalid = false;
+
+    foreach(var x in line)
+    {
+        if (startMap.ContainsKey(x))
+        {
+            chunks.Push(x);
+            continue;
+        }
+
+        var startChar = chunks.Pop();
+        if (startChar != endMap[x])
+        {
+            invalid = true;
+            break;
+        }
+    }
+
+    if (invalid) continue;
+
+    long points = 0;
+    while (chunks.TryPop(out var start))
+    {
+        points = points * 5 + pointMap[startMap[start]];
+    }
+
+    scores.Add(points);
+
+}
+
+scores.Sort();
+
+Console.WriteLine(scores[scores.Count / 2]);
